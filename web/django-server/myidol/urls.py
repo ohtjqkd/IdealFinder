13495a054555febb9db2cdc50e2146888d567f97
ohtjqkd <ohtjqkd@gmail.com>
2021-11-29 23:54:18 +0900

    Delete web/django-server/myidol directory
    이전 버전 삭제

--- web/django-server/myidol/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-# from rest_framework import views
-import myidol.views as views
-urlpatterns = [
-    path('', views.home),
-    path('process', views.process.as_view()),
-    path('result', views.Result.as_view())
-    # path('start/<int:stage>/', )
-]
